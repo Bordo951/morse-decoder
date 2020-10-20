41bfc02f379badbc6d620cba7ef04cc2a5496434
Irina Selivanova <irina.selivanova1106@gmail.com>
2020-10-20 15:01:30 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -35,10 +35,44 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+},
+    BINARY_TABLE = {
+    '10': '.',
+    '11': '-'
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrTeens = [],
+        arrFinal = [];
+
+    for (let i = 0; i < expr.length; i+=10) {
+        let element = expr.substring(i, i+10);
+        if (element !== '**********') {
+            element = parseInt(element).toString();
+        }
+        arrTeens.push(element);
+    }
+
+    for (let i = 0; i < arrTeens.length; i++) {
+        let elementArrTeens = arrTeens[i];
+        if (elementArrTeens !== '**********') {
+            arrTeens[i] = '';
+            for (let j = 0; j < elementArrTeens.length; j+=2) {
+                let keyValue = elementArrTeens.substring(j, j+2);
+                arrTeens[i] += BINARY_TABLE[keyValue];
+            }
+        }
+    }
+
+    for (let i = 0; i < arrTeens.length; i++) {
+        if (arrTeens[i] !== '**********') {
+            arrFinal.push(MORSE_TABLE[arrTeens[i]]);
+        } else {
+            arrFinal.push(' ');
+        }
+    }
+
+    return arrFinal.join('');
 }
 
 module.exports = {
